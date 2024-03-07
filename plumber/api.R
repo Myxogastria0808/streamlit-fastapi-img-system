79153ca7bab1946c176376b82de8bba3859b7cf7
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-03-07 18:59:23 +0900

    fix CORS setting

--- plumber/api.R
+++ plumber/api.R
@@ -1,6 +1,6 @@
 library(plumber)
 
-#参考: https://www.rplumber.io/articles/annotations.html
+
 # Title
 #* @apiTitle Sample API
 # Description
@@ -18,52 +18,55 @@ library(plumber)
 
 
 #* @filter cors
-cors <- function(res) {
-    res$setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:9000")
-    res$setHeader("Access-Control-Allow-Method", "GET")
-    res$setHeader("Access-Control-Allow-Headers", "*")
-        res$setHeader("Access-Control-Allow-Credentials", TRUE)
+cors <- function(req, res) {
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods", "GET")
+    res$setHeader(
+      "Access-Control-Allow-Headers",
+      req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS
+    )
+    res$status <- 200
+    return(list())
+  } else {
     plumber::forward()
+  }
 }
-
 
 #* dataディレクトリ内のsvg画像を取得できる
 #* @param generation 世代数を入力してください。
 #* @param img 取得したい画像の番号を入力してください。
 #* @serializer contentType list(type="image/svg+xml")
-#* @preempt cors
 #* @get /generation/<generation:int>/img/<img:int>
 function(generation, img) {
-    file_path <- paste0(getwd(), "/data/", generation, "/", img, ".svg")
-    readBin(file_path, "raw", n=file.info(file_path)$size)
+  file_path <- paste0(getwd(), "/data/", generation, "/", img, ".svg")
+  readBin(file_path, "raw", n = file.info(file_path)$size)
 }
 
 #* dataディレクトリ内のディレクトリの一覧を取得できる
-#* @preempt cors
 #* @get /archive
 function() {
-    root_dir <- getwd()
-    root_archive_dir <- paste0(getwd(), "/archive")
-    setwd(root_archive_dir)
-    root_archive_dirs <- dir()
-    result_list <- list()
-    for (archive_dir in root_archive_dirs) {
-        each_archive_dir <- paste0(root_archive_dir, "/", archive_dir)
-        setwd(each_archive_dir)
-        generation_dirs <- dir()
-        result_list <- append(result_list, list(c(archive_dir, generation_dirs)))
-    }
-    setwd(root_dir)
-    return(list(archive = result_list))
+  root_dir <- getwd()
+  root_archive_dir <- paste0(getwd(), "/archive")
+  setwd(root_archive_dir)
+  root_archive_dirs <- dir()
+  result_list <- list()
+  for (archive_dir in root_archive_dirs) {
+    each_archive_dir <- paste0(root_archive_dir, "/", archive_dir)
+    setwd(each_archive_dir)
+    generation_dirs <- dir()
+    result_list <- append(result_list, list(c(archive_dir, generation_dirs)))
+  }
+  setwd(root_dir)
+  return(list(archive = result_list))
 }
 
 #* dataディレクトリ内のsvg画像を取得できる
 #* @param generation 世代数を入力してください。
 #* @param img 取得したい画像の番号を入力してください。
 #* @serializer contentType list(type="image/svg+xml")
-#* @preempt cors
 #* @get /archive/<dir_name>/generation/<generation:int>/img/<img:int>
 function(dir_name, generation, img) {
-    file_path <- paste0(getwd(), "/archive/", dir_name, "/", generation, "/", img, ".svg")
-    readBin(file_path, "raw", n=file.info(file_path)$size)
+  file_path <- paste0(getwd(), "/archive/", dir_name, "/", generation, "/", img, ".svg")
+  readBin(file_path, "raw", n = file.info(file_path)$size)
 }
